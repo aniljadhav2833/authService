280e0928ed1a9ed5b73ce8991fac94c3b47460ef
aniljadhav2833 <aniljadhav2833@gmail.com>
2025-03-22 11:48:17 +0530

    init

---
+++ src/main/java/com/aniljadhav2833/dosti/auth_service/AuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aniljadhav2833.dosti.auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aniljadhav2833/dosti/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aniljadhav2833.dosti.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
